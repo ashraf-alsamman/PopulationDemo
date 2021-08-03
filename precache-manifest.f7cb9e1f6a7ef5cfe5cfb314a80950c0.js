ddeb0b59d21037d13e7bd7ac486cacd2695344e2
unknown <ashraf.alsamman@gmail.com>
2021-08-03 10:46:01 +0200

    init

---
+++ precache-manifest.f7cb9e1f6a7ef5cfe5cfb314a80950c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a0f00a2a1ebbecb9d9b6f4995b6ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e53e69196d395ee6e78",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f1f4e6ec2b787255c66",
+    "url": "/static/js/2.2cff9a28.chunk.js"
+  },
+  {
+    "revision": "9365f5e76931fbfb2338b42416d5818c",
+    "url": "/static/js/2.2cff9a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e53e69196d395ee6e78",
+    "url": "/static/js/main.b6371bbe.chunk.js"
+  },
+  {
+    "revision": "1f794f33c6c73079f7e4",
+    "url": "/static/js/runtime-main.31b196f2.js"
+  }
+]);
